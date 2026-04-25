965ea3f3a385f746574e076ea1e77c024a57be44
Mauricio Correa <maocorrea.d@gmail.com>
2026-04-25 03:59:50 -0500

    refactor: enhance getEnvVar to support process.env and additional context lookups

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,19 +1,32 @@
 export function getEnvVar(context: any, key: string): string | undefined {
+  let val: string | undefined = undefined;
+
+  try {
+    if (typeof process !== 'undefined' && process.env && process.env[key]) {
+      val = process.env[key];
+    }
+  } catch (e) {}
+  if (val) return val;
+
   try {
     if (context?.locals?.runtime?.env && context.locals.runtime.env[key]) {
-      return context.locals.runtime.env[key];
+      val = context.locals.runtime.env[key];
     }
-  } catch (err) {
-    // Getter for runtime.env might throw in certain dev environments
-  }
+  } catch (err) {}
+  if (val) return val;
+
+  try {
+    if (context?.env && context.env[key]) {
+      val = context.env[key];
+    }
+  } catch(e) {}
+  if (val) return val;
   
   try {
     if (import.meta.env && import.meta.env[key]) {
-      return import.meta.env[key];
+      val = import.meta.env[key];
     }
-  } catch (err) {
-    // Ignore
-  }
+  } catch (err) {}
 
-  return undefined;
+  return val;
 }
